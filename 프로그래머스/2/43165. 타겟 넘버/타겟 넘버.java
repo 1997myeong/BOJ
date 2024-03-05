dea773d5a351478b129e1fcd5eaff7fa57007b07
1997myeong <129561323+1997myeong@users.noreply.github.com>
2024-03-05 20:39:07 +0900

    [level 2] Title: 타겟 넘버, Time: 3.83 ms, Memory: 78.5 MB -BaekjoonHub

--- 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.java
+++ 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.java
@@ -1,26 +1,48 @@
+import java.util.*;
+
 class Solution {
-    static int answer = 0;
-    public int solution(int[] numbers, int target) {
+    
+    public static class NumInfo{
+        int num;
+        int idx;
         
-        dfs(numbers, 0, target, 0);
+        public NumInfo(int num, int idx) {
+            this.num = num;
+            this.idx = idx;
+        }
+    }
+    
+    static Queue<NumInfo> q = new LinkedList<>();
+    static int answer;
+    public int solution(int[] numbers, int target) {
+        answer = 0;
+        
+        
+        
+        bfs(0, numbers, target);   
+        
+        
+        
         return answer;
     }
     
-    public void dfs(int[] numbers, int depth, int target, int cal) {
-        if (depth == numbers.length) {
-            if(target == cal) {
-               answer++; 
+    public static void bfs(int start, int[] numbers, int target) {
+        q.add(new NumInfo(0, 0));
+        while(!q.isEmpty()) {
+            NumInfo poll = q.poll();
+            
+            if (poll.idx == numbers.length) {
+                if(poll.num == target) {
+                    answer++;
+                }
+                continue;
+            } else {
+                int temp = poll.num - numbers[poll.idx]; // 결과값
+                q.add(new NumInfo(temp, poll.idx + 1));
+                temp = poll.num + numbers[poll.idx];
+                q.add(new NumInfo(temp, poll.idx + 1));
             }
-            
-            return;
-        } else {
-            dfs(numbers, depth + 1, target, cal + numbers[depth]);
-            dfs(numbers, depth + 1, target, cal - numbers[depth]);
+
         }
-        
-    
-            
-        
-        
     }
 }
